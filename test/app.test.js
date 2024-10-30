23f7f5bd4d85a620b0de924614851b31e8937bd7
JianYuChentw <coke818572@gmail.com>
2024-10-30 13:55:32 +0800

    1.fix:jest測試後未關閉問題 2.update:package指令

--- test/app.test.js
+++ test/app.test.js
@@ -1,6 +1,15 @@
-// test/app.test.js
 const request = require('supertest');
 const app = require('../app');
+
+let server;
+
+beforeAll(() => {
+    server = app.listen(3000, () => console.log("Test server running"));
+});
+
+afterAll(done => {
+    server.close(done);  // 確保測試結束後伺服器關閉
+});
 
 describe('GET /api/message', () => {
     it('should return a JSON object with a message', async () => {
